33c99b43e2045de4223eef6b394d98cc0fdfcc90
Fakhry0 <alylolo223344@gmail.com>
2024-05-20 02:55:50 +0300

    make 7. Console 0.1

--- console.py
+++ console.py
@@ -1,29 +1,121 @@
-#!/usr/bin/python3
-"""Module for the entry point of the command interpreter."""
-
 import cmd
+from models import storage
+from models.base_model import BaseModel
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for AirBnB clone project."""
     prompt = '(hbnb) '
+    classes = {'BaseModel': BaseModel}
 
     def do_quit(self, arg):
-        """Quit command to exit the program."""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program."""
+        """Exit the program at end of file"""
+        print()  # Ensure a newline is printed before exiting
         return True
 
     def emptyline(self):
-        """Do nothing on empty input line."""
+        """Do nothing on empty input line"""
         pass
 
-    def postcmd(self, stop, line):
-        """Add an extra newline after each command execution."""
-        print()
-        return stop
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it and prints the id"""
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg not in self.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = self.classes[arg]()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class name and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        obj = storage.all().get(key)
+        if obj is None:
+            print("** no instance found **")
+        else:
+            print(obj)
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        if key not in storage.all():
+            print("** no instance found **")
+        else:
+            del storage.all()[key]
+            storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances based or not on the class name"""
+        args = shlex.split(arg)
+        objects = storage.all()
+        if len(args) > 0:
+            if args[0] not in self.classes:
+                print("** class doesn't exist **")
+                return
+            objects = {k: v for k, v in objects.items()
+                       if k.startswith(args[0])}
+        print([str(obj) for obj in objects.values()])
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding or updating attribute"""
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        obj = storage.all().get(key)
+        if obj is None:
+            print("** no instance found **")
+            return
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        if len(args) == 3:
+            print("** value missing **")
+            return
+        attr_name, attr_value = args[2], args[3]
+        try:
+            if '.' in attr_value:
+                attr_value = float(attr_value)
+            else:
+                attr_value = int(attr_value)
+        except ValueError:
+            pass
+        setattr(obj, attr_name, attr_value)
+        obj.save()
 
 
 if __name__ == '__main__':
